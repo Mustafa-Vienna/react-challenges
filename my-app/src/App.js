013f27e3fa59e08931e6dc525af3ee141cafc052
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-12-05 18:32:13 +0000

    Add eventclass.js and eventfunctional.js for learning events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import NavbarSimple from './components/NavBarSimple';
+import EventClass from './components/EventClass';
+import EventFunctional from './components/EventFunctional';
+import "./App.css";
+
 
 function App() {
   return (
-    <div className={css.App}>
+    // <div className={css.App}>
+    <div className="App">
       {/* Add your components here */}
-      <Sidebar />
-      <NavbarSimple />
+      {/* <Sidebar />
+      <NavbarSimple /> */}
+      <EventFunctional />
+      <EventClass />
     </div>
   );
 }
